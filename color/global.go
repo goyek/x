682570867cf1bd5f0d53a6ac04fe253e1451694f
Robert Pajak <pellared@hotmail.com>
2026-07-21 16:24:45 +0200

    refine NoColor comment

--- color/global.go
+++ color/global.go
@@ -16,8 +16,8 @@ func init() {
 }
 
 // NoColor prevents colorizing the output.
-// It changes process-wide state and must be called during single-threaded
-// program setup, before any goroutine can format colored output.
+// It changes process-wide state and must be called during program
+// initialization, before any goroutine can format colored output.
 func NoColor() {
 	color.NoColor = true
 }
